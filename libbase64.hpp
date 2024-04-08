40abd338448afe6e8393def808848334c4fc14bc
Qwinth <vitaliygr2301@gmail.com>
2024-04-08 11:45:01 +0300

    Update cppilbs

--- libbase64.hpp
+++ libbase64.hpp
@@ -6,38 +6,34 @@
 class Base64 {
     std::string enctable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
-    std::string enc3bytes(std::string buffer) {
-        std::string ret;
-
-        if (buffer.size()) {
+    void enc3bytes(const char* buffer, char* dest, size_t size) {
+        if (size) {
             uint8_t chr0 = buffer[0];
             uint8_t chr1 = buffer[1];
             uint8_t chr2 = buffer[2];
 
-            ret.push_back(chr0 >> 2);
+            dest[0] = chr0 >> 2;
 
-            if (buffer.size() > 1) {
-                ret.push_back((chr1 >> 4) | ((chr0 & 0x03) << 4));
+            if (size > 1) {
+                dest[1] = (chr1 >> 4) | ((chr0 & 0x03) << 4);
 
-                if (buffer.size() > 2) {
-                    ret.push_back((chr2 >> 6) | ((chr1 & 0xF) << 2));
-                    ret.push_back(chr2 & 0x3F);
+                if (size > 2) {
+                    dest[2] = (chr2 >> 6) | ((chr1 & 0xF) << 2);
+                    dest[3] = chr2 & 0x3F;
                 }
                 
                 else {
-                    ret.push_back((chr1 & 0xF) << 2);
-                    ret.push_back(0x40);
+                    dest[2] = (chr1 & 0xF) << 2;
+                    dest[3] = 0x40;
                 }
             }
             
             else {
-                ret.push_back((chr0 & 0x03) << 4);
-                ret.push_back(0x40);
-                ret.push_back(0x40);
+                dest[1] = (chr0 & 0x03) << 4;
+                dest[2] = 0x40;
+                dest[3] = 0x40;
             }
         }
-
-        return ret;
     }
 
     std::string dec3bytes(std::string buffer) {
@@ -57,16 +53,21 @@ class Base64 {
     }
 
 public:
+    size_t baseCalculate(size_t size) {
+        return ((4 * size / 3) + 3) & ~3;
+    }
 
     std::string encodeString(std::string buffer) {
         std::string ret;
 
-        for (size_t i = 0; i < buffer.size(); i += 3) {
-            std::string tmp = enc3bytes(buffer.substr(i, 3));
+        size_t base64_size = baseCalculate(buffer.size());
+        char* destbuff = new char[base64_size];
 
-            for (uint8_t i : tmp) ret += enctable[i];
-        }
+        encode(buffer.c_str(), destbuff, buffer.size());       
 
+        ret = destbuff;
+
+        delete[] destbuff;
         return ret;
     }
 
@@ -84,14 +85,28 @@ public:
         return ret;
     }
 
-    int encode(const void* buffer, void* dest, size_t size) {
-        std::string ret = encodeString(std::string((char*)buffer, size));
-        memcpy(dest, ret.c_str(), ret.size());
+    size_t encode(const char* buffer, char* dest, ssize_t size) {
+        size_t retsize = baseCalculate(size);
+        char* ret = new char[retsize];
+        char* retb = ret;
+        
+        char tmp1[3];
+        char tmp2[4];
 
-        return ret.size();
+        for (size_t i = 0; i < size; i += 3) {
+            memcpy(tmp1, &buffer[i], (size - (i + 3) >= 0) ? 3 : size - (i + 3));
+            enc3bytes(tmp1, tmp2, size);
+
+            for (int i = 0; i < 4; i++) (*(retb++)) = enctable[tmp2[i]];
+        }
+
+        memcpy(dest, ret, retsize);
+
+        delete[] ret;
+        return retsize;
     }
 
-    int decode(const void* buffer, void* dest, size_t size) {
+    int decode(const char* buffer, char* dest, size_t size) {
         std::string ret = decodeString(std::string((char*)buffer, size));
         memcpy(dest, ret.c_str(), ret.size());
 
---
+++ libcbuf.hpp
@@ -0,0 +1,105 @@
+// version 1.0
+#pragma once
+#include <cstring>
+
+class CircularBuffer {
+    size_t readIndex = 0;
+    size_t writeIndex = 0;
+
+    size_t bufferSize = 0;
+    size_t bufferSizeUsed = 0;
+    
+    char* buffer = nullptr;
+public:
+    CircularBuffer() {}
+    CircularBuffer(size_t size) {
+        resize(size);
+    }
+
+    ~CircularBuffer() {
+        delete[] buffer;
+    }
+
+    void resize(size_t size) {
+        char* newbuffer = new char[size];
+
+        memcpy(newbuffer, buffer, std::min(bufferSize, size));
+
+        delete[] buffer;
+        buffer = newbuffer;
+        bufferSize = size;
+    }
+
+    size_t read(char* dest, size_t size) {
+        if (!bufferSizeUsed) return 0;
+
+        size_t destIndex = 0;
+        size_t readsize = std::min(size, bufferSizeUsed);
+        size_t retsize = readsize;
+
+        if (readIndex + readsize >= bufferSize) {
+            memcpy(&dest[destIndex], &buffer[readIndex], bufferSize - readIndex);
+
+            destIndex += bufferSize - readIndex;
+            readsize -= bufferSize - readIndex;
+            bufferSizeUsed -= bufferSize - readIndex;
+            readIndex = 0;
+        }
+
+        memcpy(&dest[destIndex], &buffer[readIndex], readsize);
+
+        bufferSizeUsed -= readsize;
+        readIndex += readsize;
+
+        return retsize;
+    }
+
+    size_t write(const char* buff, size_t size) {
+        if (bufferSizeUsed == bufferSize) return 0;
+
+        size_t bufferSizeFree = bufferSize - bufferSizeUsed;
+        size_t writesize = std::min(size, bufferSizeFree);
+        size_t retsize = writesize;
+        size_t buffIndex = 0;
+
+        if (writeIndex + writesize > bufferSize) {
+            memcpy(&buffer[writeIndex], &buff[buffIndex], bufferSize - writeIndex);
+
+            buffIndex += bufferSize - writeIndex;
+            writesize -= bufferSize - writeIndex;
+            bufferSizeUsed += bufferSize - writeIndex;
+            writeIndex = 0;
+        }
+
+        memcpy(&buffer[writeIndex], &buff[buffIndex], writesize);
+
+        bufferSizeUsed += writesize;
+        writeIndex += writesize;
+
+        if (writeIndex == bufferSize) writeIndex = 0;
+
+        return retsize;
+    }
+
+    size_t usage() {
+        return bufferSizeUsed;
+    }
+
+    size_t size() {
+        return bufferSize;
+    }
+
+    void clear() {
+        readIndex = 0;
+        writeIndex = 0;
+        bufferSizeUsed = 0;
+    }
+
+    bool full() {
+        return bufferSizeUsed == bufferSize;
+    }
+
+    bool empty() {
+        return !bufferSizeUsed;
+    }
+};
